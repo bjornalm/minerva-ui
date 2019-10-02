c3f7e2f408e725466dfb7627718494cb799ec0cd
Bjorn Alm <bjorn.b.alm@gmail.com>
2019-10-02 15:53:54 +0200

    refactor main rendering

--- src/App.js
+++ src/App.js
@@ -5,19 +5,25 @@ import MinervaParser from "./MinervaParser";
 import Rect from "./components/Rect";
 import Circle from "./components/Circle";
 import Line from "./components/Line";
+import { MINERVA } from "./helpers";
 
 class App extends Component {
   state = {};
 
   shapeDragDropped = move => {
-    console.info(move.modified);
-    const update = MinervaParser.buildDragDropQuery(
-      move.original,
-      move.modified
-    );
+    // const update = MinervaParser.buildDragDropQuery(
+    //   move.original,
+    //   move.modified
+    // );
+
+    const { original, modified } = move;
+    const shapesCopy = [...this.state.shapes];
+    const shapeIndex = shapesCopy.indexOf(original);
+    shapesCopy.splice(shapeIndex, 1, modified);
+
     this.setState({
       ...this.state,
-      ...update
+      shapes: shapesCopy
     });
   };
 
@@ -31,15 +37,62 @@ class App extends Component {
       .then(data => {
         const primitives = MinervaParser.buildPrimitives(data);
         this.setState({
-          rectangle: primitives[0],
-          line: primitives[1],
-          circle: primitives[2]
+          shapes: primitives
         });
       });
   }
 
+  renderShapes() {
+    if (!this.state.shapes) {
+      return null;
+    }
+    const renderedShapes = this.state.shapes.map(shape => {
+      const key = shape.atomId;
+      let renderedShape;
+      switch (shape.form.type) {
+        case MINERVA.PRIMITIVES.CIRCLE:
+          renderedShape = (
+            <Circle
+              key={key}
+              onDragDropped={this.shapeDragDropped}
+              shape={shape}
+            ></Circle>
+          );
+          break;
+        case MINERVA.PRIMITIVES.RECTANGLE:
+          renderedShape = (
+            <Rect
+              key={key}
+              onDragDropped={this.shapeDragDropped}
+              shape={shape}
+            ></Rect>
+          );
+          break;
+        case MINERVA.PRIMITIVES.LINE:
+          renderedShape = (
+            <Line
+              key={key}
+              onDragDropped={this.shapeDragDropped}
+              shape={shape}
+            ></Line>
+          );
+          break;
+        default:
+          console.error("Shape could not be rendered", shape);
+          break;
+      }
+
+      return renderedShape;
+    });
+
+    return renderedShapes;
+  }
+
   render() {
     console.info(this.state);
+    const containerStyle = {
+      boxShadow: "5px 5px 10px -2px rgba(0,0,0,0.22)"
+    };
     return (
       <div className="App">
         <h1>Minerva</h1>
@@ -50,20 +103,10 @@ class App extends Component {
           height="800"
           viewBox="0 0 100 100"
           xmlns="http://www.w3.org/2000/svg"
+          style={containerStyle}
         >
-          <rect width="100%" height="100%" fill="black" />
-          <Circle
-            onDragDropped={this.shapeDragDropped}
-            shape={this.state.circle}
-          ></Circle>
-          <Rect
-            onDragDropped={this.shapeDragDropped}
-            shape={this.state.rectangle}
-          ></Rect>
-          <Line
-            onDragDropped={this.shapeDragDropped}
-            shape={this.state.line}
-          ></Line>
+          <rect width="100%" height="100%" fill="#eeeeee" />
+          {this.renderShapes()}
         </svg>
       </div>
     );
